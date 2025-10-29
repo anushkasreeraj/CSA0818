15a530d0d2991ba90d063fcbff7438031c2a73e1
Anushka Sree Raj <anushkasreerajofficial@gmail.com>
2025-10-29 10:59:19 +0530

    Add number sign classification in CSA0818-4.2.py

---
+++ CSA0818-4.2.py
@@ -0,0 +1,9 @@
+#+ve or -ve
+
+n=int(input('enter a number'))
+if n>0:
+    print('the given no. is positive')
+elif n<0:
+    print('the given no. is negative')
+else:
+    print('the given no. is zero')
